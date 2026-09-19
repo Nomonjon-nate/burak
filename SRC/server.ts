6b6a1ea7f071cb30fe51a6f2a7d29c8128109540
Nomonjon-nate <begmatovnomonjon6@gmail.com>
2026-09-19 20:20:35 +0900

    feat: updated

--- SRC/server.ts
+++ SRC/server.ts
@@ -1,9 +1,15 @@
-console.log("EXECUTED");
+// console.log("EXECUTED");
 
-import moment from 'moment';
+// import moment from 'moment';
 
-const currentTime = moment().format('YYYY MM DD');
-console.log(currentTime);
+// const currentTime = moment().format('YYYY MM DD');
+// console.log(currentTime);
 
-const person: string = "Martin";
-const count: number = 100;
+// const person: string = "Martin";
+// const count: number = 100;
+
+// Architectural pattern: MVC, Dependency Injection (backend), MVP (frontend)
+
+// MVC = MODEL VIEW CONTROLLER
+
+// Design pattern: Middleware, Decorator
